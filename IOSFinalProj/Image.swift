5a8d0304e6edb5eaed837d746f25faf1a7c0980b
Ye Eun Lee <117133205+JumboYogurt@users.noreply.github.com>
2024-06-14 17:19:52 +0900

    Data

---
+++ IOSFinalProj/Image.swift
@@ -0,0 +1,15 @@
+//
+//  Image.swift
+//  MasterDetail
+//
+//  Created by mac022 on 2024/06/11.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+func resized(to size: CGSize) -> UIImage {
+return UIGraphicsImageRenderer(size: size).image { _ in draw(in: CGRect(origin: .zero, size: size))
+} }
+}
